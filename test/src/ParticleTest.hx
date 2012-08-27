77d313e73a0f95c42d39536ae27117c8dc2dbf34
profelis <system.grand@gmail.com>
2012-08-27 21:14:52 +0300

    minor

--- test/src/ParticleTest.hx
+++ test/src/ParticleTest.hx
@@ -1,39 +1,26 @@
 package ;
 
+import deep.dd.display.Sprite2D;
+import mt.m3d.Color;
+import flash.geom.Vector3D;
+import flash.display.BitmapData;
 import deep.dd.particle.ParticleSystem2D;
 import deep.dd.particle.preset.GravityParticlePreset;
-import deep.dd.display.render.BatchRender;
 import deep.dd.particle.render.GravityParticleRender;
-import deep.dd.display.render.SimpleRender;
-import deep.dd.display.render.CloudRender;
-import deep.dd.display.SmartSprite2D;
 import deep.dd.particle.ParticleSystem2D;
 import deep.dd.particle.render.GravityParticleRender.GPUGravityParticleRender;
 import deep.dd.particle.render.GravityParticleRender;
 import deep.dd.particle.preset.ParticlePresetBase;
 import deep.dd.particle.preset.ParticlePresetBase.Bounds;
-import deep.dd.utils.Stats;
-import deep.dd.animation.Animator;
-import flash.geom.Vector3D;
-import deep.dd.texture.atlas.parser.SpriteSheetParser;
-import deep.dd.texture.atlas.parser.StarlingParser;
-import deep.dd.texture.atlas.AtlasTexture2D;
-import deep.dd.display.MovieClip2D;
-import deep.dd.display.Quad2D;
-import deep.dd.utils.GlobalStatistics;
-import mt.m3d.Color;
 import deep.dd.utils.BlendMode;
-import deep.dd.display.Sprite2D;
-import flash.utils.ByteArray;
-import deep.dd.texture.Texture2D;
-import flash.display.BitmapData;
-import flash.events.MouseEvent;
+import deep.dd.utils.Stats;
 import flash.display.StageAlign;
 import flash.display.StageScaleMode;
 import deep.dd.display.Scene2D;
 import flash.display3D.Context3DRenderMode;
 import deep.dd.World2D;
 import flash.events.Event;
+import flash.events.MouseEvent;
 
 @:bitmap("deep.png") class Image extends BitmapData {}
 
@@ -64,18 +51,18 @@ class ParticleTest
         var texture = world.cache.getTexture(Image);
 
         preset = new GravityParticlePreset();
-        preset.particleNum = 1000;
-        preset.spawnNum = 15;
-        preset.spawnStep = 0.02;
+        preset.particleNum = 15000;
+        preset.spawnNum = 100;
+        preset.spawnStep = 0.03;
         preset.life = new Bounds<Float>(2, 3);
-        preset.startPosition = new Bounds<Vector3D>(new Vector3D(0, 0, 0), new Vector3D(400, 0, 0));
+        preset.startPosition = new Bounds<Vector3D>(new Vector3D(0, 0, 0), new Vector3D(600, 0, 0));
         preset.velocity = new Bounds<Vector3D>(new Vector3D(30, 0, 0), new Vector3D(40, 10, 0));
         preset.startColor = new Bounds<Color>(new Color(1, 1, 1, 0.6));
         preset.endColor = new Bounds<Color>(new Color(0, 0, 0, 0.01), new Color(1, 1, 1, 0.01));
         preset.gravity = new Vector3D(0, 100, 0);
         preset.startScale = new Bounds<Float>(0.5);
         preset.endScale = new Bounds<Float>(0.05);
-        preset.startRotation = new Bounds<Vector3D>(new Vector3D(0, 0, 45));
+        preset.startRotation = new Bounds<Vector3D>(new Vector3D(0, 0, 0), new Vector3D(0, 0, 360));
 
 
         //var ps = new ParticleSystem2D(new GravityParticleRender(preset, new CloudRender()));
